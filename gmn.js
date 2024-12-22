07523787a6af801512faf8d233e8fb8f20c57b37
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-12-22 21:00:32 +0100

    hta academy class

--- gmn.js
+++ gmn.js
@@ -31,10 +31,12 @@ async function displayRandomUser() {
     }
     const data = await response.json();
     const user = data.results[0];
+    console.log(user);
 
     console.log(`Name: ${user.name.first} ${user.name.last}`);
     console.log(`Email: ${user.email}`);
     console.log(`Location: ${user.location.city}, ${user.location.country}`);
+    console.log(user.location.street.number);
     console.log(`Profile Picture: ${user.picture.large}`);
   } catch (error) {
     console.error("Error fetching random user:", error);
